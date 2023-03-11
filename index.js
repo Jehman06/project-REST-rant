e43dec5f3c8b82aff1ab7b3963abf9efe7c63db5
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-10 17:10:34 -0800

    Added a 404 page route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+require('dotenv').config()
 let express = require('express')
 let app = express()
 
@@ -6,4 +7,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
