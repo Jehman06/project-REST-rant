08c2b56de15711c34a94f37ad4b0c58630679f11
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-10 17:16:14 -0800

    Added places controller

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 let express = require('express')
 let app = express()
 
+app.use('/places', require('/controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
