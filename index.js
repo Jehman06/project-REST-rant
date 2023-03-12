7412b497ed4ec5d0e6461d2ce59f85dac374a470
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-12 13:27:34 -0700

    Adding home page view

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ require('dotenv').config()
 let express = require('express')
 let app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
