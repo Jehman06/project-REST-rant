0fb9d1a20e6bfeff3bdb47422e20d3ba04254ac1
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-15 21:47:09 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-restaurant.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coding-cat-cafe.jpeg'
+  }] 
