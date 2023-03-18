fb8e3eedca44724778dc175197a35bc8ed94c2aa
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-17 19:08:48 -0700

    Delete functionality implemented

---
+++ views/places/edit.jsx
@@ -0,0 +1,38 @@
+const React = require('react')
+const Def = require('../default')
+
+function edit_form() {
+    return (
+        <Def>
+            <main>
+                <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" value={data.place.name} required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" type="url" id="pic" name="pic" value={data.place.pic}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" value={data.place.city}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" value={data.place.state}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required/>
+                    </div>
+                    <br/>
+                    <input className="btn btn-primary" type="submit" value="Update Place"/>
+                </form>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,45 @@
 const React = require('react')
 const Def = require('../default')
 
-function Show() {
+function Show(data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <br/>
+                <div className="row">
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name}/>
+                    </div>
+                    <div className='col-sm-6'>
+                        <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <p>No Ratings yet.</p>
+                        <h2>Description</h2>
+                        <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-sm'>
+                                    <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                                        Edit
+                                    </a>
+                                </div>
+                                <div className='col-sm'>
+                                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                                        <button type="submit" className='btn btn-danger'>
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <hr/>
+                <div className='container-fluid'>
+                    <h1>Comments</h1>
+                    <p>No comments yet.</p>
+                </div>
             </main>
         </Def>
     )
