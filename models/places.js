f98176c0b948f57b36753648d979cf3d4895dc4b
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-22 14:05:06 -0700

    Added Place Schema

--- models/places.js
+++ models/places.js
@@ -1,15 +1,27 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai-restaurant.jpg',
-    foundedYear: '1998'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coding-cat-cafe.jpeg',
-    foundedYear: '2006'
-  }] 
+const mongoose = require('mongoose')
+const placeSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  city: {type: String, default: 'Anytown'},
+  state: {type: String, default: 'USA'},
+  cuisines: {type: String, required: true},
+  pic: {type: String, default: 'https://d.newsweek.com/en/full/1519658/barcito-los-angeles.jpg?w=1600&h=1600&q=88&f=64b0d307cded2a8be3187114f44f175b'},
+  foundedYear: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai-restaurant.jpg',
+//     foundedYear: '1998'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/coding-cat-cafe.jpeg',
+//     foundedYear: '2006'
+//   }] 
