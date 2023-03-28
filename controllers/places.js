3cc7ae39b189bcf3310edee73c9d5abd898a73ee
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-27 19:37:22 -0700

    Can add comments and see them on the Show Page

--- controllers/places.js
+++ controllers/places.js
@@ -95,8 +95,25 @@ router.get('/:id/edit', (req, res) => {
     })
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+    .then(place => {
+      db.Comment.create(req.body)
+        .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+            .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+          res.render('error404')
+        })
+    })
+    .catch(err => {
+      res.render('error404')
+    })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -67,6 +67,31 @@ function Show(data) {
                     <h1>Comments</h1>
                     {comments}
                 </div>
+                <hr/>
+                <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-check-input" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
             </main>
         </Def>
     )
